53b5505f812578f28d4604c0b2d344f8e6ad0bd4
Alex Clark <aclark@aclark.net>
2020-03-29 16:34:55 -0400

    Update

--- assets/js/events.js
+++ assets/js/events.js
@@ -8,8 +8,7 @@ function fetchGroups(url, cb, data) {
         method: 'get',
         url: url,
         success: function(result) {
-            console.log('back with ' + result.data.length + ' results');
-            console.dir(result);
+            // console.dir(result);
             //add to data
             data.push.apply(data, result.data);
             if (result.meta.next_link) {
@@ -28,13 +27,11 @@ $(document).ready(function() {
     var $results = $("#events");
 
     fetchGroups("https://api.meetup.com/dcpython/events?&sign=true&photo-host=public&page=20", function(res) {
-        console.log("totally done");
-        console.dir(res);
-
+        // console.dir(res);
         var s = "";
         for (var i = 0; i < 3; i++) {
             var ev = res[i];
-            s += "<div class='card border my-5'><h1 class='card-header'>" + ev.name + "</h1><div class='card-body text-left'><h5 class='card-title'>" + ev.description + "</h5></div> <a target='_blank' href='" + ev.link + "' class='btn btn-primary'>Go somewhere</a> </div> </div>";
+            s += "<div class='card border my-5'><h1 class='card-header'>" + ev.name + "</h1><div class='card-body text-left'><h5 class='card-title'>" + ev.description + "</h5></div> <a target='_blank' href='" + ev.link + "' class='btn btn-primary'>Go to event</a> </div> </div>";
         }
         $results.html(s);
 
