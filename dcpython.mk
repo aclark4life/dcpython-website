3795dc05aff749470ad540616c5b72e95f6c427c
Alex Clark <aclark@aclark.net>
2022-01-18 14:06:47 -0500

    Update

--- dcpython.mk
+++ dcpython.mk
@@ -1,5 +1,5 @@
 eb-deploy:
-	aws --profile=dc s3 cp --recursive --exclude "lib/*" . s3://dev.dcpython.org --exclude "node_modules/*" --exclude ".git/*" --exclude Makefile --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+	aws --profile=dc s3 cp --recursive --exclude "lib/*" --exclude "bin/*" . s3://dev.dcpython.org --exclude "node_modules/*" --exclude ".git/*" --exclude Makefile --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
 pack:
 	./node_modules/.bin/webpack
