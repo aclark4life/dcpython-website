c9028fa6f17d9bd669c7464b96cff929095d0321
Alex Clark <aclark@aclark.net>
2022-02-06 11:21:36 -0500

    Update

--- dcpython.mk
+++ dcpython.mk
@@ -1,6 +1,6 @@
 eb-deploy:
 	aws --profile=dc s3 cp --recursive --exclude "lib/*" --exclude "bin/*" . s3://dev.dcpython.org --exclude "node_modules/*" --exclude ".git/*" --exclude Makefile --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-	aws cloudfront create-invalidation --distribution-id E13BTW6RNYVM9H -paths "/*"
+	aws cloudfront create-invalidation --distribution-id E13BTW6RNYVM9H --paths "/*"
 
 pack:
 	./node_modules/.bin/webpack
