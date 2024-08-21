45da05cc0b4d40c914833a7b9a973dffbd210f12
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-21 13:21:03 -0400

    Add/update project.mk

--- project.mk
+++ project.mk
@@ -1,14 +1,4 @@
-PROJECT_NAME := dcpython-website
-
-deploy-prod:
-	aws --profile=dc s3 cp --recursive --exclude "lib/*" --exclude "bin/*" . s3://dcpython.org --exclude "node_modules/*" --exclude ".git/*" --exclude Makefile --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-
-deploy-dev:
-	aws --profile=dc s3 cp --recursive --exclude "lib/*" --exclude "bin/*" . s3://dev.dcpython.org --exclude "node_modules/*" --exclude ".git/*" --exclude Makefile --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-	aws --profile=dc cloudfront create-invalidation --distribution-id E13BTW6RNYVM9H --paths "/*"
-
-pack:
-	./node_modules/.bin/webpack
-
-eb-deploy:
-	$(MAKE) deploy-prod
+# Custom Makefile
+# Add your custom makefile commands here
+#
+# PROJECT_NAME := my-new-project
